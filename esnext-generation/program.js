11b4d33702e3af22a4df91ac35631adea551db8d
humancatfood <janborchers09@googlemail.com>
2015-10-29 00:50:37 +0000

    [+] some more challenges

--- esnext-generation/program.js
+++ esnext-generation/program.js
@@ -1,4 +1,5 @@
 /*global console:true*/
+/*global module:true*/
 /*global process:true*/
 
 (function (f) {
@@ -7,7 +8,6 @@
 
     var functions = [
 
-
         /**
          * initial
          *
@@ -15,9 +15,10 @@
          */
         function challenge1 () {
 
-             module.exports = function makeCounter(someObj, max = 10) {
+             module.exports = function makeCounter(someObj, max) {
 
                  var it = 1, done = false;
+                 max = max || 10;
 
                  someObj.next = function() {
 
@@ -38,13 +39,69 @@
 
              };
 
+        },
+
+
+        /**
+         * Use for (var i of iterable) { .. } to filter any iterable.
+         *
+         */
+        function challenge2 () {
+
+            module.exports = function filterForNumbers(iterable) {
+
+                var result = [];
+
+                // loop over iterable, adding numeric values to a new array
+                // then return the new array of numbers
+                for (var item of iterable)
+                {
+                    if (typeof item === 'number')
+                    {
+                        result.push(item);
+                    }
+                }
+
+                return result;
+
+            };
 
         },
 
+
+        /**
+         * Generate editable iterator
+         *
+         */
+        function challenge3 () {
+
+            // return an Iterator for even numbers if isEven is true
+            // or, return an Iterator for odd numbers if isEven is false
+            // If `.next(swap)` receives `true`, swap between even <-> odd
+            module.exports = function generate(isEven) {
+
+                var num = isEven ? 0 : -1;
+
+                return {
+
+                    next: function next(swap) {
+
+                        num += swap ? 1 : 2;
+                        return {
+                            value: num
+                        };
+
+                    }
+
+                };
+
+            };
+
+        }
 
     ];
 
 
     functions[f - 1].call();
 
-}( 1 ));
+}( 3 ));
