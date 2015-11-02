8f61238074d652dc84d9b8a14e55f491002514a3
HumanCatfood <me@humancatfood.com>
2015-11-02 20:22:52 +0000

    [+] started some learnyoureact

---
+++ learnyoureact/program.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.argv[2] || 3000));
+app.set('view engine', 'jsx');
+app.set('views', __dirname + '/views');
+app.engine('jsx', require('express-react-views').createEngine());
+
+require('node-jsx').install();
+
+app.use('/', function(req, res) {
+    'use strict';
+    res.render('index', '');
+});
+
+app.listen(app.get('port'), function() {});
---
+++ learnyoureact/views/index.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var TodoBox = React.createClass({
+    render: function() {
+        return (
+            <div className="todoBox">
+                <h1>Todos</h1>
+
+            </div>
+        );
+    }
+});
+
+module.exports = TodoBox;
