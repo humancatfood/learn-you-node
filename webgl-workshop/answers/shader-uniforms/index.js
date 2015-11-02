4671134cf5fce6d0105ee0607cfdd7aa4c16a47b
humancatfood <janborchers09@googlemail.com>
2015-11-02 12:20:44 +0000

    [+] added some more webgl challenges (what's with the missing semicolons??)

--- webgl-workshop/answers/shader-uniforms/index.js
+++ webgl-workshop/answers/shader-uniforms/index.js
@@ -1,33 +1,44 @@
 //Code for setting up shader program is provided for you
-var setupShader  = require('./setup-shader')
+var setupShader  = require('./setup-shader');
 
 //As before, this draws a triangle
-var drawTriangle = require('./draw-triangle')
+var drawTriangle = require('./draw-triangle');
 
 //These are the values at which you should draw the triangles
-var scales  = [ 0.15, 0.25, 0.5, 0.3 ]
-var offsets = [ [-0.5,-0.5], 
-                [0.5,-0.5], 
+var scales  = [ 0.15, 0.25, 0.5, 0.3 ];
+var offsets = [ [-0.5,-0.5],
+                [0.5,-0.5],
                 [0.1,0.3],
-                [0.3,-0.4] ]
+                [0.3,-0.4] ];
 var colors  = [ [1, 0, 0],
                 [0, 1, 0],
                 [0, 0, 1],
-                [1, 1, 0] ]
+                [1, 1, 0] ];
 
 //This is the program object
-var program
+var program, scaleLocation, colorLocation, offsetLocation;
 
 exports.init = function(gl) {
-  program = setupShader(gl)
+    program = setupShader(gl);
 
-  //TODO: Get uniform locations here
-}
+    scaleLocation = gl.getUniformLocation(program, 'scale');
+    offsetLocation = gl.getUniformLocation(program, 'offset');
+    colorLocation = gl.getUniformLocation(program, 'color');
+
+};
 
 exports.draw = function(gl) {
-  gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight)
-  gl.clearColor(0,0,0,1)
-  gl.clear(gl.COLOR_BUFFER_BIT)
+    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+    gl.clearColor(0,0,0,1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
-  //TODO: Draw the triangles with the shader/uniforms
-}
+    gl.useProgram(program);
+
+    for (var i = 0; i < scales.length; i += 1)
+    {
+        gl.uniform1f(scaleLocation, scales[i]);
+        gl.uniform2fv(offsetLocation, offsets[i]);
+        gl.uniform3fv(colorLocation, colors[i]);
+        drawTriangle(gl);
+    }
+};
